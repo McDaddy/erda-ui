e4ac0b9f8ccd96d0f98aeaaac75f3abeb037ef8a
陈韡韬 <mcdaddychen@126.com>
2021-05-24 07:45:53 +0000

    add config

--- .devcontainer/scripts/install-dev-tools.sh
+++ .devcontainer/scripts/install-dev-tools.sh
@@ -1,9 +1,9 @@
-echo installing dev tools
+echo start installing dev tools
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-echo git clone done
+echo git clone zsh plugins done
 
 sed -i "73c plugins=(git z zsh-syntax-highlighting zsh-autosuggestions)" ~/.zshrc
 sed -i '73a ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#afffaf,bg=cyan,bold,underline"' ~/.zshrc
@@ -13,4 +13,8 @@ echo "zsh" >> ~/.bashrc
 source ~/.bashrc
 source ~/.zshrc
 
+echo finish overriding .zshrc
+
 git config core.editor vi
+
+echo finish change default git editor
